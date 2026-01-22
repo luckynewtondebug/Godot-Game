f3716699ed08dc0a067d65834a03d8307bc34fa8
luckynewtondebug <pbigham2028@crossroadscollegeprep.org>
2026-01-22 15:31:01 -0600

    coins and worldmap

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
+var DOUBLE_JUMP = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,7 +13,13 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	#Handle double jump.
+	if Input.is_action_just_pressed("jump") and !is_on_floor() and DOUBLE_JUMP!=1:
+		velocity.y =JUMP_VELOCITY
+		DOUBLE_JUMP = 1
+	#Reset double jump.
+	if is_on_floor():
+		DOUBLE_JUMP = 0
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
